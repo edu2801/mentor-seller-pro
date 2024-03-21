cf1b46020a5e242e90834267f62392bad5a1ac3b
Eduardo Silva <eduardo.luiz.s@hotmail.com>
2024-03-21 20:16:26 -0300

    feat: amazon-advertise-model

---
+++ app/Models/AmazonAdvertise.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AmazonAdvertise extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'item_id',
+        'external_sku',
+        'title',
+        'thumbnail',
+        'variation',
+        'parent_sku',
+        'permalink',
+        'price',
+        'sold_quantity',
+        'visits',
+        'account_id'
+    ];
+}
